cc162cf9bd271460634f0e20a2ce51a53aa17130
Arpan Kumar <kumararpan187@gmail.com>
2024-07-19 22:30:57 +0530

    conversionOperation added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// to get value in  number
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let b = "55abs";
+// let b = null; // value=0 type=object
+// let b = true; //value=1 ,if false then 0 ,type boolean
+// let b = undefined; // type=> undefined ,value =>NaN
+let value = Number(b);
+// console.log(typeof value);
+// console.log(typeof b);
+// console.log(value); // NaN
+
+let isLoggedIn = "arpan";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn); //  => boolean
+// console.log(booleanIsLoggedIn); //  => true
+
+// ""  => false
+// 0 => false
+// 1 => true
+// "arpan" => true
+
+let number = 33;
+let StringNumber = String(number);
+
+// console.log(typeof StringNumber);  // => String
+// console.log(StringNumber);      // =>33
