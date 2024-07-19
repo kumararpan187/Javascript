770859e8daa33f31794a7a66e885d25bcf3dcc45
Arpan Kumar <kumararpan187@gmail.com>
2024-07-19 22:51:55 +0530

    operations added

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,3 +32,32 @@ let StringNumber = String(number);
 
 // console.log(typeof StringNumber);  // => String
 // console.log(StringNumber);      // =>33
+
+/********************************************operations**********************************************************/
+
+let num = 3;
+let negNum = -num;
+// console.log(negNum)   =>  -3
+
+// console.log(2 + 2); // => 4
+// console.log(2 - 2); // => 0
+// console.log(2 * 2); // => 4
+// console.log(2 / 2); // => 1
+// console.log(2 ** 3); // => 8
+// console.log(3 % 2); // => 0  gives remainder
+
+let str1 = "Arpan";
+let str2 = " Kumar";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // => 12
+// console.log(1 + "2"); // => 12
+// console.log("1" + 2 + 2); // => 122
+// console.log(1 + 2 + "2"); // =>32
+
+// console.log(+""); // => 0
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter); // => 101
