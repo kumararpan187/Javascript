3c63d822bb86cdb77ffbf37dfe0e525565741675
Arpan Kumar <kumararpan187@gmail.com>
2024-07-19 20:32:26 +0530

    datatypes added

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const id = 8564;
+
+let name = "Arpan";
+
+var surName = "Kumar";
+
+fullName = "Arpan Kumar";
+
+console.log(surName);
+console.log([id, name, surName, fullName]);
+
+// prefer not to use var due to issue in block scope and functional issue
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //traet all js code as newer version
+
+//alert(3+3)   error because  we are using nodejs, not browser;
+
+//code readability should be high
+
+let user = "Arpan";
+
+let id = 18;
+
+let active = true;
+
+console.table([user, id, active]);
+
+// Number;
+// BigInt;
+// Boolean=> true, false;
+// String => "";
+// null => standalone value ;
+// undefined
+//symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
