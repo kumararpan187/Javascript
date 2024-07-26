2e8525f504841dc6297a5a42b1435cc7dab5e6c1
Arpan Kumar <kumararpan187@gmail.com>
2024-07-26 10:48:56 +0530

    filename updated

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -2,7 +2,7 @@ if (true) {
     let a = 10;
     let b = 20;
     const c = 30;
-    // var d = 40; // never use var  creates scope problem 
+    // var d = 40; //   never use var   because it creates scope problem 
     console.log(`inner scope value of a : ${a}`);
 }
 let a = 100;
