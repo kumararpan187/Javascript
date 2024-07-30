23d0eeb5b70a31d574255e0290fbe2c5044374d5
Arpan Kumar <kumararpan187@gmail.com>
2024-07-30 08:52:27 +0530

    updated

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if 
+
+// if (condition) {
+//     // code
+// }
+
+
+const temperature = 31;
+
+// if (temperature >= 35) {
+//     console.log(`temperature : ${temperature} , it's hot today`);
+// } else {
+//     console.log(`temperature ${temperature} , it's not hot today`);
+// }
+
+
+
+const score = 200;
+// if (score > 100) {
+//     const power = "fly";
+//     console.log(`user power ${power}`);
+// }
+// console.log(`user power ${power}`); // error power is not defined 
+
+
+const balance = 1200;
+
+// if (balance < 500) {
+//     console.log(`balance is less than 500`);
+// } else if (balance < 750) {
+//     console.log(`balnce is less then 750`);
+// } else if (balance < 900) {
+//     console.log(`balance is less than 1000`);
+// } else {
+//     console.log(`balance is greater than 1000`);
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInfromGoogle = true;
+const loggedInfromEmail = true;
+
+// if (userLoggedIn && debitCard) {
+//     console.log(`Allowed to buy card `);
+// }
+
+// if (loggedInfromEmail || loggedInfromGoogle) {
+//     console.log(`user logged in`);
+// }
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,38 @@
+// falsy values 
+// =>  false , 0 , -0 , NaN , null , undefined , BigInt 0n ,"" (empty string)
+
+
+// truthy values 
+//  => [] , 1 , "0" , 'false' ," " , {} , function (){}
+
+
+
+// false == 0 => true
+// flase == "" => true
+// 0 == '' => true 
+
+
+// to check if array is empty 
+const arr = [];
+// if (arr.length === 0) {
+//     console.log("array is empty");
+// }
+
+
+// to check if object is empty 
+const obj = {}
+// if (Object.keys(obj).length === 0) {
+//     console.log(`Object is empty`);
+// }
+
+// Nullish Coalescing Operator (??): nnull undefined
+
+
+
+
+// Ternary Operator
+//condition ? true:false
+
+const iceTeaPrice = 60
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
